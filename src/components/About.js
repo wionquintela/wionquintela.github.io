34ccf2f594cfd9af6df33c350ac6a364fd926291
wion quintela <118152735+wionquintela@users.noreply.github.com>
2022-11-26 14:48:22 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -16,11 +16,7 @@ export default function About() {
             <h1 className="wow fadeInUp text-center" style={h1Style}>About the Creator</h1>
                 <div className="row row-cols-1 row-cols-md-2">
                 <div className="col">
-<<<<<<< HEAD
                         <img src={require("./img/propayl.jpg")} className="img-fluid p-1 wow fadeInLeft" style={pic}/>
-=======
-                        <img src="img/propayl.jpg" className="img-fluid p-1 wow fadeInLeft" style={pic}/>
->>>>>>> 5b17e7e4b6a0d7954824d049d87e53ffbb48a8cc
                     </div>
                     <div className="col">
                 <p className="wow fadeInRight pt-3">My name is Wion M. Quintela, and I became interested in programming back in January out of curiosity. I started programming in Python and learn from watching in YouTube. Usually when I am not coding, I play my guitar and bass, watch TV shows, play Steam Games like TF2 and Brawlhalla, listen to some good music including Arctic Monkeys and Drake, and occasionally create and publish my animations on YouTube. Currently, I am focusing on my studies since I am now in Senior High School and I need to be always learning because it is different from Junior High School which was more laid-back, but the subjects now are more vigorous and mind-melting, and I hate Gen Chem.</p>
