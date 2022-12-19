d1ce5ce6c4a25907000a869acaf6af807b9fa425
rocketexpert <qmw9849@gmail.com>
2022-12-19 09:37:50 +0800

    added IG link and polished navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,11 +14,12 @@ export default function Footer({ }) {
                 <a href="https://www.youtube.com/channel/UC4WftbieTcS90rvqLTguyZA" className="sociallink">YouTube</a>
                 <a href="https://github.com/rocketexpert" className="sociallink">GitHub</a>
                 <Link className="sociallink" to="/">Home</Link> */}
-                <p className="sociallink  wow fadeIn">Developed by Wion Quintela @ 2022</p>
+                <p className="sociallink  wow fadeIn">Developed by Wion Quintel @ 2022</p>
                 <div className="mysocmedlinks">
                   <a href="https://web.facebook.com/wion.quintela06/" className="footerlink"><i class="fa-brands fa-facebook"></i></a>
                   <a href="https://www.youtube.com/channel/UC4WftbieTcS90rvqLTguyZA" className="footerlink"><i class="fa-brands fa-youtube"></i></a> 
                   <a href="https://github.com/rocketexpert" className="footerlink"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.instagram.com/wayonnn/" className="footerlink"><i class="fa-brands fa-instagram"></i></a>
                   <a href="mailto:wionquintela06@gmail.com" className="footerlink"><i class="fa-solid fa-envelope"></i></a> 
                   </div>
                       {/* <div className="row row-cols-1">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,70 +1,31 @@
+
+
+
+
 export default function Nav() {
     return (
-        <div>
-            {/* <nav className="navbar navbar-expand-lg navbar-light">
-  <div className="container-fluid">
-    <a className="text-light navbar-brand npy" href="#">Navbar</a>
-    <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse text-center" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item gx-2">
-          <a className="navlink active" aria-current="page" href="#skills">Skillset</a>
-        </li>
-        <li className="nav-item gx-2">
-          <a className="navlink" href="#about">About</a>
-        </li>
-        <li className="nav-item gx-2">
-          <a className="navlink" href="#projects">Projects</a>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Wion Quintela</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#skills">Skillset</a>
           </li>
-          <li className="nav-item gx-2">
-          <a className="navlink" href="#footer">Links</a>
+          <li class="nav-item">
+            <a class="nav-link active" href="#about">About</a>
           </li>
-      </ul>
-    </div>
-  </div>
-</nav> */}
-<nav className="navbar navbar-dark bg-dark fixed-top">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Wion Quintela</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Navigation</h5>
-        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <a className="nav-link sociallink" aria-current="page" href="#skills">Skillset</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link sociallink" href="#about">About Me</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link sociallink" aria-current="page" href="#projects">Projects</a>
-          </li>
-          <li className="nav-item dropdown">
-            {/* <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul className="dropdown-menu dropdown-menu-dark">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr className="dropdown-divider"/>
-              </li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul> */}
+          <li class="nav-item">
+            <a class="nav-link active" href="#projects">Projects</a>
           </li>
         </ul>
       </div>
     </div>
-  </div>
-</nav>
-</div>
+  </nav> 
+    </div>
     )
 }
